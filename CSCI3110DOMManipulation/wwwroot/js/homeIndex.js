50708b33890759beb9db1c3ec07406f5e6a9e768
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2024-03-30 17:14:16 -0400

    Redid part 1

--- CSCI3110DOMManipulation/wwwroot/js/homeIndex.js
+++ CSCI3110DOMManipulation/wwwroot/js/homeIndex.js
@@ -10,51 +10,66 @@ console.log(p2);
 let p3 = document.querySelector("p#basic");
 console.log(p3);
 
-let button = document.querySelector("#btnClickMe");
-button.addEventListener("click", (e) => {
+// Event delegation - the listener is attached to the document.
+// This is effective when working with dynamic DOM elements.
+document.addEventListener("click", (e) => {
+    if (e.target.getAttribute("id") === "btnClickMe") {
+        displayElements();
+    } else if (e.target.getAttribute("id") === "btnClickMeToo") {
+        changeTheDOM();
+    }
+});
+
+function displayElements() {
     console.log(`Before: ${p3.textContent}`);
     p3.textContent = p3.textContent + " CHANGED!";
     console.log(`After button click: ${p3.textContent}`);
-});
 
-let paragraphs = document.querySelectorAll("p");
+    let paragraphs = document.querySelectorAll("p");
 
-console.log('querySelectorAll("p")');
-console.log('indexed for');
-for (let i = 0; i < paragraphs.length; i++) {
-    let p = paragraphs[i];
-    console.log(p);
+    console.log('querySelectorAll("p")');
+    console.log('indexed for');
+    for (let i = 0; i < paragraphs.length; i++) {
+        let p = paragraphs[i];
+        console.log(p);
+    }
+
+    console.log('for of');
+    for (let p of paragraphs) {
+        console.log(p);
+    }
+
+    console.log('forEach');
+    paragraphs.forEach((p) => {
+        console.log(p);
+    });
 }
 
-console.log('for of');
-for (let p of paragraphs) {
-    console.log(p);
+function changeTheDOM() {
+    console.log("CHANGING THE DOM!");
+    // Select the div with class text-center
+    let mainDiv = document.querySelector("div.text-center");
+    // Append a new paragraph
+    mainDiv.innerHTML += "<p>Added Paragraph</p>";
+
+    // Create the paragraph
+    const newP = document.createElement("p");
+    // Create the content of the paragraph
+    const newText = document.createTextNode("This is new text.");
+    // Add the next text node to the paragraph
+    newP.appendChild(newText);
+    // Add the new paragraph to the div
+    mainDiv.appendChild(newP);
+
+    // Create the DOM parser
+    const domParser = new DOMParser();
+    // Parse the HTML
+    const doc = domParser.parseFromString(
+        "<p>Added Paragraph using DOM parser</p>", "text/html");
+    // Get the paragraph from the created document
+    const newP2 = doc.querySelector("p");
+    mainDiv.appendChild(newP2);
+    console.log("DONE CHANGING THE DOM!");
 }
 
-console.log('forEach');
-paragraphs.forEach((p) => {
-    console.log(p);
-});
 
-// Select the div with class text-center
-let mainDiv = document.querySelector("div.text-center");
-// Append a new paragraph
-mainDiv.innerHTML += "<p>Added Paragraph</p>";
-
-// Create the paragraph
-const newP = document.createElement("p");
-// Create the content of the paragraph
-const newText = document.createTextNode("This is new text.");
-// Add the next text node to the paragraph
-newP.appendChild(newText);
-// Add the new paragraph to the div
-mainDiv.appendChild(newP);
-
-// Create the DOM parser
-const domParser = new DOMParser();
-// Parse the HTML
-const doc = domParser.parseFromString(
-    "<p>Added Paragraph using DOM parser</p>", "text/html");
-// Get the paragraph from the created document
-const newP2 = doc.querySelector("p");
-mainDiv.appendChild(newP2);
